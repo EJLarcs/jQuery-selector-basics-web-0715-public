2359cb0eb6d56f97b4e4490db789e4a5ab6ce1c7
EJLarcs <ejlarcombe@gmail.com>
2015-09-10 19:27:54 -0400

    complete

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,26 @@
 'use-strict';
-$(document).ready(function(){
-  h1Selector();
-  liInOlSelector();
-  linkSelector();
-  imageSelector();
-  checkboxInputSelector();
-});
 
-// declare your functions here...
+function h1Selector() {
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+}
+
+function linkSelector(){
+  return $('#box4 a');
+}
+
+function imageSelector(){
+  return $('img[alt ="cat sleeping"]');
+}
+
+
+function checkboxInputSelector(){
+  return $('input:checkbox').wrap('<span class="pinky"></span>');
+}
+
+$(document).ready(function(){
+
+});
